8077dbf2b58a6fe3d93cd73a404549aebc74d972
Daniel Mohr <daniel.mohr@dlr.de>
2021-07-27 18:38:39 +0200

    enhanced coding style

--- gitolite_web_interface.py
+++ gitolite_web_interface.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author: Daniel Mohr.
-Date: 2021-05-11, 2021-06-08, 2021-06-09 (last change).
+Date: 2021-05-11, 2021-06-08, 2021-06-09, 2021-07-27 (last change).
 License: GNU GENERAL PUBLIC LICENSE, Version 2, June 1991.
 
 [gitolite](https://gitolite.com/gitolite/) is a great tool to manage
@@ -26,8 +26,8 @@ for managing user accounts with password, this script
 *gitolite_web_interface.py* provides the possibility to let the users
 manage their ssh keys in a gitolite environment.
 
-To use this script, you have to adapt the following few variables in the last
-if clause (at the end of the file):
+To use this script, you have to adapt the following few keys of the global
+variable CONFIG:
 
   * gitolite_wrapper_script
   * ssh_gitolite_user
@@ -36,6 +36,8 @@ if clause (at the end of the file):
 
 Further there is an option to let the user create repositories (no wild repo).
 
+You can also overide the CONFIG variable in the last if clause
+(at the end of the file).
 """
 
 import cgi
@@ -44,6 +46,32 @@ import subprocess
 import tempfile
 
 DEBUG = True
+
+CONFIG = {
+    # define the script, which is called when using gitolite over http
+    # as described in: https://gitolite.com/gitolite/contrib/ssh-and-http
+    'gitolite_wrapper_script': '/var/www/bin/gitolite-suexec-wrapper.sh',
+    # define the hosting user of your gitolite installation
+    # as described in https://gitolite.com/gitolite/quick_install.html
+    'ssh_gitolite_user': 'gitolite',
+    # define the ssh host as used in a possible ssh comand;
+    # if None, it will be set to HTTP_HOST:
+    'ssh_host': None,
+    # define if only https traffic is accaptable (True or False):
+    'only_https': True,
+    # define the gitolite command used on command line
+    'gitolite_cmd': 'gitolite',
+    # define the gitolite home directory
+    'gitolite_home': '/srv/gitolite',
+    # define the gitolite admin repository path in the gitolite home
+    'gitolite_admin_repo': 'repositories/gitolite-admin.git',
+    # define, which options should be provided:
+    'provided_options': {
+        'help': True,
+        'info': True,
+        'mngkey': True,
+        'createrepo': False}
+}
 
 # pylint: disable=missing-docstring
 
@@ -112,17 +140,17 @@ def gitolite_web_interface(
     sskm_help_link += 'target="_blank">'
     sskm_help_link += 'changing keys -- self service key management</a></p>'
     if (('QUERY_STRING' in os.environ.keys()) and
-            (len(os.environ['QUERY_STRING']) > 0)):
+            (bool(os.environ['QUERY_STRING']))):
         if ((os.environ['QUERY_STRING'] in ['help', 'info']) and
                 (provided_options[os.environ['QUERY_STRING']])):
-            cp = subprocess.run(
+            cpi = subprocess.run(
                 [gitolite_wrapper_script],
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 shell=True, timeout=3, check=True)
             content = '<h1>gitolite command (' + \
                 os.environ['QUERY_STRING'] + ')</h1>\n'
             content += '<h2>Output:</h2>'
-            content += '<pre>' + cp.stdout.decode() + '</pre>'
+            content += '<pre>' + cpi.stdout.decode() + '</pre>'
             title = 'gitolite command (' + os.environ['QUERY_STRING'] + ')'
             output(content=content, title=title)
             exit(0)
@@ -136,11 +164,11 @@ def gitolite_web_interface(
                 content += '<h2>Your current keys are:</h2>'
                 new_env = os.environ.copy()
                 new_env["QUERY_STRING"] = "sskm list"
-                cp = subprocess.run(
+                cpi = subprocess.run(
                     [gitolite_wrapper_script],
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                     shell=True, timeout=3, check=False, env=new_env)
-                content += '<pre>' + cp.stdout.decode() + '</pre>'
+                content += '<pre>' + cpi.stdout.decode() + '</pre>'
                 # create form
                 content += '<h2>Your new ssh key:</h2>'
                 content += '<p>Paste your public ssh key, which is typically '
@@ -175,7 +203,7 @@ def gitolite_web_interface(
                     exit(0)
                 new_env = os.environ.copy()
                 new_env["QUERY_STRING"] = 'sskm add ' + form["name"].value
-                cp = subprocess.run(
+                cpi = subprocess.run(
                     [gitolite_wrapper_script],
                     input=form["pubkey"].value.encode(),
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
@@ -184,15 +212,15 @@ def gitolite_web_interface(
                 content = '<h1>manage ssh keys with sskm (added)</h1>'
                 content += sskm_help_link
                 content += '<h2>Output:</h2>'
-                content += '<pre>' + cp.stdout.decode() + '</pre>'
+                content += '<pre>' + cpi.stdout.decode() + '</pre>'
                 content += '<h2>Your ssh keys:</h2>'
                 new_env = os.environ.copy()
                 new_env["QUERY_STRING"] = "sskm list"
-                cp = subprocess.run(
+                cpi = subprocess.run(
                     [gitolite_wrapper_script],
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                     shell=True, timeout=3, check=False, env=new_env)
-                content += '<pre>' + cp.stdout.decode() + '</pre>'
+                content += '<pre>' + cpi.stdout.decode() + '</pre>'
                 content += '<h2>Next Step:</h2>'
                 content += '<p>Now you have to verify your ssh key (by ssh). '
                 content += 'You cannot continue with the web interface.</p>'
@@ -218,20 +246,20 @@ def gitolite_web_interface(
                 os.environ['QUERY_STRING'].replace('%20', ' ') + ')</h1>\n'
             content += sskm_help_link
             content += '<h2>Output:</h2>'
-            cp = subprocess.run(
+            cpi = subprocess.run(
                 [gitolite_wrapper_script],
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 shell=True,
                 timeout=3, check=True)
-            content += '<pre>' + cp.stdout.decode() + '</pre>'
+            content += '<pre>' + cpi.stdout.decode() + '</pre>'
             content += '<h2>Your current keys are:</h2>'
             new_env = os.environ.copy()
             new_env["QUERY_STRING"] = "sskm list"
-            cp = subprocess.run(
+            cpi = subprocess.run(
                 [gitolite_wrapper_script],
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 shell=True, timeout=3, check=False, env=new_env)
-            content += '<pre>' + cp.stdout.decode() + '</pre>'
+            content += '<pre>' + cpi.stdout.decode() + '</pre>'
             title = os.environ['QUERY_STRING'].replace('%20', ' ')
             output(content=content, title=title)
             exit(0)
@@ -263,18 +291,18 @@ def gitolite_web_interface(
                 new_env = os.environ.copy()
                 new_env["HOME"] = gitolite_home
                 my_cmd = gitolite_cmd + ' list-memberships -u ' + user
-                cp = subprocess.run(
+                cpi = subprocess.run(
                     my_cmd,
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                     shell=True, timeout=3, check=True, env=new_env)
-                groups = cp.stdout.splitlines()
+                groups = cpi.stdout.splitlines()
                 names = set()
                 for i in range(len(groups)):
                     groups[i] = groups[i][1:]
-                    ap, pn = groups[i].split(b'_', maxsplit=1)
-                    if ap == b'owner':
-                        names.add(pn)
-                content += '<pre>' + cp.stdout.decode() + '</pre>\n'
+                    access, username = groups[i].split(b'_', maxsplit=1)
+                    if access == b'owner':
+                        names.add(username)
+                content += '<pre>' + cpi.stdout.decode() + '</pre>\n'
                 content += '<h2>You can create repositories for:</h2>\n'
                 content += '<pre>' + b','.join(names).decode() + '</pre>\n'
                 content += '<h2>New repo:</h2>\n'
@@ -289,8 +317,8 @@ def gitolite_web_interface(
                 content += '<td>project directory: </td>'
                 content += '<td>'
                 content += '<select name="project" size="1">'
-                for pn in names:
-                    content += '<option>' + pn.decode() + '</option>'
+                for username in names:
+                    content += '<option>' + username.decode() + '</option>'
                 content += '</select>'
                 content += '</td>'
                 content += '</tr><tr>'
@@ -315,17 +343,17 @@ def gitolite_web_interface(
                 new_env = os.environ.copy()
                 new_env["HOME"] = gitolite_home
                 my_cmd = gitolite_cmd + ' list-memberships -u ' + user
-                cp = subprocess.run(
+                cpi = subprocess.run(
                     my_cmd,
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                     shell=True, timeout=3, check=True, env=new_env)
-                groups = cp.stdout.splitlines()
+                groups = cpi.stdout.splitlines()
                 names = set()
                 for i in range(len(groups)):
                     groups[i] = groups[i][1:]
-                    ap, pn = groups[i].split(b'_', maxsplit=1)
-                    if ap == b'owner':
-                        names.add(pn)
+                    access, username = groups[i].split(b'_', maxsplit=1)
+                    if access == b'owner':
+                        names.add(username)
                 if not form["project"].value.encode() in names:
                     content = '<p>You cannot create a repo in this project "'
                     content += form["project"].value.encode() + '".</p>'
@@ -350,20 +378,20 @@ def gitolite_web_interface(
                                                           gitolite_admin_repo)
                     admin_repo_name = os.path.splitext(
                         os.path.split(gitolite_admin_repo)[1])[0]
-                    cp = subprocess.run(
+                    subprocess.run(
                         git_cmd,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                         shell=True, cwd=tmpdir, timeout=3, check=True,
                         env=new_env)
                     git_cmd = 'git config user.name "' + user + '"'
-                    cp = subprocess.run(
+                    subprocess.run(
                         git_cmd,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                         shell=True, cwd=os.path.join(tmpdir, admin_repo_name),
                         timeout=3, check=True, env=new_env)
                     git_cmd = 'git config user.email "' + user + '@'
                     git_cmd += os.environ.get('HTTP_HOST') + '"'
-                    cp = subprocess.run(
+                    subprocess.run(
                         git_cmd,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                         shell=True, cwd=os.path.join(tmpdir, admin_repo_name),
@@ -372,14 +400,13 @@ def gitolite_web_interface(
                         tmpdir, admin_repo_name, 'conf/gitolite.conf')
                     with open(conf_path, 'r') as fd:
                         gitolite_config = fd.read().splitlines()
-                    new_config = []
                     repo_group_def = False
                     repo_group_def_index = None
                     repo_def = False
-                    it = 0
+                    iterartion = 0
                     for line in gitolite_config:
                         if line.startswith(repo_group):
-                            repo_group_def_index = it
+                            repo_group_def_index = iterartion
                             if repo_path in line:
                                 repo_group_def = True
                         elif (line.startswith('repo ') and
@@ -387,7 +414,7 @@ def gitolite_web_interface(
                             repo_def = True
                         if repo_group_def and repo_def:
                             break
-                        it += 1
+                        iterartion += 1
                     if not repo_group_def:
                         repo_group_def_str = repo_group + ' = ' + repo_path
                         if repo_group_def_index is not None:
@@ -420,7 +447,7 @@ def gitolite_web_interface(
                     with open(conf_path, 'w') as fd:
                         fd.write('\n'.join(gitolite_config))
                     git_cmd = 'git add conf/gitolite.conf'
-                    cp = subprocess.run(
+                    cpi = subprocess.run(
                         git_cmd,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                         shell=True, cwd=os.path.join(tmpdir, admin_repo_name),
@@ -431,10 +458,10 @@ def gitolite_web_interface(
                         content += git_cmd
                         content += '</pre>'
                         content += '<pre>'
-                        content += cp.stdout.decode()
+                        content += cpi.stdout.decode()
                         content += '</pre>'
                     git_cmd = 'git commit -m "added repo ' + repo_path + '"'
-                    cp = subprocess.run(
+                    cpi = subprocess.run(
                         git_cmd,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                         shell=True, cwd=os.path.join(tmpdir, admin_repo_name),
@@ -445,12 +472,12 @@ def gitolite_web_interface(
                         content += git_cmd
                         content += '</pre>'
                         content += '<pre>'
-                        content += cp.stdout.decode()
+                        content += cpi.stdout.decode()
                         content += '</pre>'
                     git_cmd = 'git push'
                     git_cmd = 'pwd;ls;cat .git/config;git push'
                     git_cmd = 'gitolite push'
-                    cp = subprocess.run(
+                    cpi = subprocess.run(
                         git_cmd,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                         shell=True, cwd=os.path.join(tmpdir, admin_repo_name),
@@ -461,10 +488,10 @@ def gitolite_web_interface(
                         content += git_cmd
                         content += '</pre>'
                         content += '<pre>'
-                        content += cp.stdout.decode()
+                        content += cpi.stdout.decode()
                         content += '</pre>'
                         content += '<pre>'
-                        content += cp.stderr.decode()
+                        content += cpi.stderr.decode()
                         content += '</pre>'
                 content += '<h2>Repository access:</h2>\n'
                 content += '<ul>'
@@ -496,44 +523,22 @@ def gitolite_web_interface(
 
 
 if __name__ == "__main__":
-    # define the script, which is called when using gitolite over http
-    # as described in: https://gitolite.com/gitolite/contrib/ssh-and-http
-    gitolite_wrapper_script = '/var/www/bin/gitolite-suexec-wrapper.sh'
-    # define the hosting user of your gitolite installation
-    # as described in https://gitolite.com/gitolite/quick_install.html
-    ssh_gitolite_user = 'gitolite'
-    # define the ssh host as used in a possible ssh comand;
-    # if None, it will be set to HTTP_HOST:
-    ssh_host = None
-    # define if only https traffic is accaptable (True or False):
-    only_https = True
-    # define the gitolite command used on command line
-    gitolite_cmd = 'gitolite'
-    # define the gitolite home directory
-    gitolite_home = '/srv/gitolite'
-    # define the gitolite admin repository path in the gitolite home
-    gitolite_admin_repo = 'repositories/gitolite-admin.git'
-    # define, which options should be provided:
-    provided_options = {
-        'help': True,
-        'info': True,
-        'mngkey': True,
-        'createrepo': False}
-    # special setting for lx117:
-    gitolite_wrapper_script = '/srv/www/bin/gitolite-suexec-wrapper.sh'
-    ssh_gitolite_user = 'git'
-    provided_options = {
+    # special setting:
+    CONFIG['gitolite_wrapper_script'] = \
+        '/srv/www/bin/gitolite-suexec-wrapper.sh'
+    CONFIG['ssh_gitolite_user'] = 'git'
+    CONFIG['provided_options'] = {
         'help': True,
         'info': True,
         'mngkey': True,
         'createrepo': True}
     # call the main program:
     gitolite_web_interface(
-        gitolite_wrapper_script,
-        ssh_gitolite_user,
-        ssh_host=ssh_host,
-        only_https=only_https,
-        gitolite_cmd=gitolite_cmd,
-        gitolite_home=gitolite_home,
-        gitolite_admin_repo=gitolite_admin_repo,
-        provided_options=provided_options)
+        CONFIG['gitolite_wrapper_script'],
+        CONFIG['ssh_gitolite_user'],
+        ssh_host=CONFIG['ssh_host'],
+        only_https=CONFIG['only_https'],
+        gitolite_cmd=CONFIG['gitolite_cmd'],
+        gitolite_home=CONFIG['gitolite_home'],
+        gitolite_admin_repo=CONFIG['gitolite_admin_repo'],
+        provided_options=CONFIG['provided_options'])
